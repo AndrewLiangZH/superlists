ce1e8c3c28053a9510bd7ac428c3a55d8897f05a
arnoidliang <arnoidliang@tencent.com>
2019-07-29 19:34:00 +0800

    Add the 2nd step in method POST

--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html', {
+        # 'new_item_text': request.POST['item_text']
+        'new_item_text': request.POST.get('item_text', '')
+    })
 # return HttpResponse('<html><title>To-Do lists</title></html>')
